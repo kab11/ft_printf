b47256ff345772031ca0d8664821b448edcf2209
Mingyun KIM <mikim@e1z2r5p19.42.us.org>
2017-04-27 18:57:49 -0700

    a

--- srcs/print_wchar.c
+++ srcs/print_wchar.c
@@ -6,45 +6,20 @@
 /*   By: mikim <mikim@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/17 16:47:27 by mikim             #+#    #+#             */
-/*   Updated: 2017/04/27 18:33:14 by mikim            ###   ########.fr       */
+/*   Updated: 2017/04/27 18:57:40 by mikim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	put_wchar(t_env *e, char c)
+void	put_wchar(t_env *e, int c)
 {
 	write(e->fd, &c, 1);
 }
 
-void	put_wc(t_env *e, wchar_t c)
-{
-	if (c <= 0x7F)
-		put_wchar(e, c);
-	else if (c <= 0x7FF)
-	{
-		put_wchar(e, (c >> 6) + 0xC0);
-		put_wchar(e, (c & 0x3F) + 0x80);
-	}
-	else if (c <= 0xFFFF)
-	{
-		put_wchar(e, ((c >> 12) + 0xE0));
-		put_wchar(e, ((c >> 6) & 0x3F) + 0x80);
-		put_wchar(e, (c & 0x3F) + 0x80);
-	}
-	else if (c <= 0x10FFFF)
-	{
-		put_wchar(e, (c >> 18) + 0xF0);
-		put_wchar(e, ((c >> 12) & 0x3F) + 0x80);
-		put_wchar(e, ((c >> 6) & 0x3F) + 0x80);
-		put_wchar(e, (c & 0x3F) + 0x80);
-	}
-	e->ret++;
-}
-
 void	print_wchar_minus(t_env *e, wchar_t wc)
 {
-	put_wc(e, wc);
+	put_wchar(e, (int)wc);
 	while (e->flag.width-- > 1)
 		e->ret += write(e->fd, " ", 1);
 }
@@ -58,7 +33,7 @@ void	print_wchar(t_env *e, wchar_t wc)
 		while (e->flag.width-- > 1)
 			e->ret += (e->flag.zero ?
 			write(e->fd, "0", 1) : write(e->fd, " ", 1));
-		put_wc(e, wc);
+		put_wchar(e, (int)wc);
 	}
 	e->i++;
 }
