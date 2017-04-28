59b67e33a6e77a1f2afce9381492badc5528f020
Mingyun KIM <mikim@e1z2r5p19.42.us.org>
2017-04-27 22:19:21 -0700

    a

--- srcs/parse_spec.c
+++ srcs/parse_spec.c
@@ -6,7 +6,7 @@
 /*   By: mikim <mikim@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/07 03:25:42 by mikim             #+#    #+#             */
-/*   Updated: 2017/04/27 19:49:28 by mikim            ###   ########.fr       */
+/*   Updated: 2017/04/27 22:19:03 by mikim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,16 @@ void	get_spec_more(const char *restrict fmt, t_env *e)
 	if (fmt[e->i] == 'o' || fmt[e->i] == 'b' || fmt[e->i] == 'x' ||
 			fmt[e->i] == 'O' || fmt[e->i] == 'B' || fmt[e->i] == 'X')
 		spec_base(e, fmt[e->i]);
-	else if (fmt[e->i] == 'n')
-		spec_return(e);
-	else if (fmt[e->i] == 'p' || fmt[e->i] == 'P')
-		spec_ptraddr(e, fmt[e->i]);
-	else if (fmt[e->i] == 'r')
-		spec_non_printable(e);
-	else if (fmt[e->i] == 'w')
-		spec_fd(e);
-	else if (fmt[e->i] == 'q')
-		print_dice(e);
+//	else if (fmt[e->i] == 'n')
+//		spec_return(e);
+//	else if (fmt[e->i] == 'p' || fmt[e->i] == 'P')
+//		spec_ptraddr(e, fmt[e->i]);
+//	else if (fmt[e->i] == 'r')
+//		spec_non_printable(e);
+//	else if (fmt[e->i] == 'w')
+//		spec_fd(e);
+//	else if (fmt[e->i] == 'q')
+//		print_dice(e);
 	else if (fmt[e->i] != '\0')
 		print_invalid_spec(e, fmt[e->i]);
 }
@@ -56,9 +56,9 @@ void	get_spec(const char *restrict fmt, t_env *e)
 	else if (((fmt[e->i] == 'c' || fmt[e->i] == 's') && e->mod == l) ||
 			fmt[e->i] == 'C' || fmt[e->i] == 'S')
 		spec_wchar(e, fmt[e->i]);
-	else if ((fmt[e->i] >= 'e' && fmt[e->i] <= 'g') || fmt[e->i] == 'a' ||
-			(fmt[e->i] >= 'E' && fmt[e->i] <= 'G') || fmt[e->i] == 'A')
-		spec_precision(e, fmt[e->i]);
+//	else if ((fmt[e->i] >= 'e' && fmt[e->i] <= 'g') || fmt[e->i] == 'a' ||
+//			(fmt[e->i] >= 'E' && fmt[e->i] <= 'G') || fmt[e->i] == 'A')
+//		spec_precision(e, fmt[e->i]);
 	else
 		get_spec_more(fmt, e);
 }
